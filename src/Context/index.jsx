9cf0fe0aaecbf6428df1d21079b603799e85f6fb
albertoaguilera31 <123708306+albertoaguilera31@users.noreply.github.com>
2023-12-09 00:42:35 -0300

    se añadieron las funcionalidades del modal de detalles del  producto

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -5,8 +5,24 @@ export const ShoppingCartContext= createContext()
 export const ShoppingCartProvider = ({children})=>{
     const [count, setCount] = useState(0)
 
+    //Product details * pen/close
+    const [isProductDetailOpen, setIsProductDetailOpen] = useState(false)
+    const openProductDetail = ()=> setIsProductDetailOpen(true)
+    const closeProductDetail = ()=> setIsProductDetailOpen(false)
+
+    //Product details * show product
+    const [productToShow, setProductToShow] = useState({})
+
     return(
-        <ShoppingCartContext.Provider value={{count, setCount}}>
+        <ShoppingCartContext.Provider value={{
+            count, 
+            setCount,
+            openProductDetail,
+            closeProductDetail,
+            isProductDetailOpen,
+            productToShow,
+            setProductToShow
+            }}>
             {children}
         </ShoppingCartContext.Provider>
     )
