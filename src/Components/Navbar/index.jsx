09c47cfc93a7dee70c65e9d00f83ede64c65d8c7
albertoaguilera31 <123708306+albertoaguilera31@users.noreply.github.com>
2023-12-14 22:49:49 -0300

    se añadio el fitro por categorias para los productos de la tienda

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -21,6 +21,7 @@ const Navbar = () =>{
                 <li>
                     <NavLink 
                     to='/'
+                    onClick={()=> context.setSearchByCategory()}
                     className={({isActive})=>
                     isActive ? activeStyle: undefined
                     }>
@@ -30,6 +31,7 @@ const Navbar = () =>{
                 <li>
                     <NavLink 
                     to='/clothes'
+                    onClick={()=> context.setSearchByCategory('clothes')}
                     className={({isActive})=>
                     isActive ? activeStyle: undefined
                     }>
@@ -39,6 +41,7 @@ const Navbar = () =>{
                 <li>
                     <NavLink 
                     to='/electronic'
+                    onClick={()=> context.setSearchByCategory('electronic')}
                     className={({isActive})=>
                     isActive ? activeStyle: undefined
                     }>
@@ -48,6 +51,7 @@ const Navbar = () =>{
                 <li>
                     <NavLink 
                     to='/furniture'
+                    onClick={()=> context.setSearchByCategory('furniture')}
                     className={({isActive})=>
                     isActive ? activeStyle: undefined
                     }>
@@ -57,6 +61,7 @@ const Navbar = () =>{
                 <li>
                     <NavLink 
                     to='/toys'
+                    onClick={()=> context.setSearchByCategory('toys')}
                     className={({isActive})=>
                     isActive ? activeStyle: undefined
                     }>
@@ -66,6 +71,7 @@ const Navbar = () =>{
                 <li>
                     <NavLink 
                     to='/others'
+                    onClick={()=> context.setSearchByCategory('others')}
                     className={({isActive})=>
                     isActive ? activeStyle: undefined
                     }>
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -10,11 +10,17 @@ function Home() {
 
   const renderView=()=>{
     if(context.searchByTitle?.length > 0){
+      if(context.filteredItems?.length>0){
       return(
         context.filteredItems?.map(item=>(
           <Card key={item.id} data={item}/>
         ))
       )
+      }else{
+        return(
+          <div>We don't have anything</div>
+        )
+      }
     }else{
       return(
         context.items?.map(item=>(
