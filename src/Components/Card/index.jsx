94769ab1533fd7dc6163f258bce798e2480c98ad
albertoaguilera31 <123708306+albertoaguilera31@users.noreply.github.com>
2023-12-21 22:09:53 -0300

    se añadieron los estilos finales al proyecto

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -40,9 +40,9 @@ const Card =(data)=>{
 
     return(
         <div
-         className='bg-white cursor-pointer w-56 h-60 rounded-lg border border-orange-500'
+         className='bg-white cursor-pointer w-56 h-60 rounded-lg border-2 border-orange-500'
          onClick={()=>showProduct(data.data)}>
-            <figure className='relative mb-2 w-full h-4/5'>
+            <figure className='relative mb-2 w-full h-4/6'>
                 <span className='absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5'>{data.data.category.name}</span>
                 <img className='w-full h-full object-cover rounded-lg' src={data.data.image} alt={data.data.title}/>
                 {renderIcon(data.data.id)}
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -26,12 +26,12 @@ function Home() {
   return (
     <Layout>
       <div className='flex items-center justify-center relative w-80 mb-4'>
-        <h1 className='font-medium text-xl'>Exclusive Products</h1>
+        <h1 className='font-medium text-xl mt-2'>Exclusive Products</h1>
       </div>
       <input
        type="text" 
        placeholder="Search a product"
-       className="rounded-lg border border-black w-80 p-4 mb-4 focus:outline-none"
+       className="rounded-lg border border-2 border-black w-80 p-4 mb-4 focus:outline-none"
        onChange={(event)=> context.setSearchByTitle(event.target.value)}
        ></input>
       <div className='grid gap-4 grid-cols-4 w-full max-w-screen-lg'>
